219328a1aaa36f08cd1c079da612a52f3a37dff3
fairytree <szhang.bc@gmail.com>
2025-07-11 20:37:01 -0700

    change absolute paths to relative paths in launch file; commented swarmtal_msgs

--- src/drone_circle_pkg/launch_drone_circle.sh
+++ src/drone_circle_pkg/launch_drone_circle.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-# Terminal 0: Prepare Gazebo (uncomment the below line if Gazebo won't reopen)
-# gnome-terminal -- bash -c "pkill -f gz; exec bash"
+# --- PARAMETERS ---
+# the top-level folder of this project:
+export SETUP_PATH="../../install/setup.bash"
+# command to execute the QGroundContorl AppImage:
+export Q_GROUND_CONTROL_CMD="../../../QGroundControl-x86_64.AppImage"
+# the following parameter should be "MicroXRCEAgent" if built from source and
+# "micro-xrce-dds-agent" if installed from the snap package
+export MICRO_XRCE_DDS_AGENT_CMD="MicroXRCEAgent"
+# location of the PX4_Autopilot folder:
+export PX4_AUTOPILOT_DIR="../../../PX4-Autopilot"
+
+# Terminal 0: main terminal
+# uncomment if Gazebo won't reopen
+source $SETUP_PATH
+pkill -f gz
 
 # Terminal 1: PX4 SITL
-gnome-terminal -- bash -c "export DISPLAY=:0; sleep 2; cd ~/PX4-Autopilot && PX4_GZ_WORLD=baylands make px4_sitl gz_x500_depth; exec bash" # To set a different sim environment, change PX4_GZ_WORLD=$simulation environment$, currently set to baylands. Drone model is set to gz_x500_depth, output are depth images and RGB images
+# To set a different sim environment, change PX4_GZ_WORLD=$simulation environment$, currently set to baylands.
+# Drone model is set to gz_x500_depth, output are depth images and RGB images
+gnome-terminal -- bash -c "source $SETUP_PATH; sleep 2; cd ~/code/Drone_Nav/PX4-Autopilot && PX4_GZ_WORLD=baylands make px4_sitl gz_x500_depth"
 
 # Terminal 2: MicroXRCEAgent
-gnome-terminal -- bash -c "MicroXRCEAgent udp4 -p 8888; exec bash"
+gnome-terminal -- bash -c "$MICRO_XRCE_DDS_AGENT_CMD udp4 -p 8888"
 
 # Terminal 3: QGroundControl
-gnome-terminal -- bash -c "~/QGroundControl-x86_64.AppImage; exec bash"
+gnome-terminal -- bash -c "$Q_GROUND_CONTROL_CMD"
 
 # Terminal 4: ROS 2 Node
-gnome-terminal -- bash -c "cd ~/drone_gazebo_Simulation_with_Vins_FUEL && source install/setup.bash && source ./install/setup.bash && sleep 30s && ros2 run drone_circle_pkg drone_circle_node; exec bash" # can modify the file "drone_circle_node.cpp" to switch between hovering motion and circling motion.
+# modify the file "drone_circle_node.cpp" to switch between hovering motion and circling motion
+gnome-terminal -- bash -c "source $SETUP_PATH && sleep 30s && ros2 run drone_circle_pkg drone_circle_node"
 
 # Terminal 5: ROS 2 Parameter Bridge
-gnome-terminal -- bash -c "sleep 31s && ros2 run ros_gz_bridge parameter_bridge \
+gnome-terminal -- bash -c "source $SETUP_PATH && sleep 31s && ros2 run ros_gz_bridge parameter_bridge \
 /world/baylands/model/x500_depth_0/link/camera_link/sensor/IMX214/image@sensor_msgs/msg/Image@ignition.msgs.Image \
 /camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo \
 /depth_camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked \
 /depth_camera@sensor_msgs/msg/Image@gz.msgs.Image \
-/world/baylands/model/x500_depth_0/link/base_link/sensor/imu_sensor/imu@sensor_msgs/msg/Imu[gz.msgs.IMU; exec bash" # publish ros topic from gazebo
+/world/baylands/model/x500_depth_0/link/base_link/sensor/imu_sensor/imu@sensor_msgs/msg/Imu[gz.msgs.IMU" # publish ros topic from gazebo
 
 # Terminal 6: Odometry Converter Node
-gnome-terminal -- bash -c "ros2 run px4_odometry_converter odom_converter_node; exec bash" # convert px4 odometry message to ros message
+# convert px4 odometry message to ros message
+gnome-terminal -- bash -c "source $SETUP_PATH && ros2 run px4_odometry_converter odom_converter_node"
